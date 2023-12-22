303f9c3a3a435d2ea7a7d01dc523941ab1a39b60
Daniel Rodriguez <maieutiquer@gmail.com>
2023-12-22 12:52:14 +0200

    upgrade deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2024: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
