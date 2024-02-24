c795b92874ab3ef1143fec72cd3f2f32422e7d40
Daniel Rodriguez <maieutiquer@gmail.com>
2024-02-24 23:52:18 +0200

    add csv button

--- src/dances/DancesList.tsx
+++ src/dances/DancesList.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation } from '@tanstack/react-query'
 import { Grid } from '../components/Grid'
 import { useState } from 'react'
 import { ColDef } from 'ag-grid-community'
@@ -9,11 +9,26 @@ export const DancesList = () => {
     queryFn: () => fetch(`${import.meta.env.VITE_API_URL}/dances`).then((res) => res.json()),
   })
 
+  const { mutate } = useMutation({
+    mutationFn: () => fetch(`${import.meta.env.VITE_API_URL}/dances/csv`, { method: 'post' }).then((res) => res.json()),
+  })
+
   const [colDefs] = useState<ColDef[]>([{ field: 'id' }, { field: 'name' }])
 
   if (isPending) return 'Loading...'
 
   if (error) return 'An error has occurred: ' + error.message
 
-  return <Grid rowData={data} colDefs={colDefs} />
+  return (
+    <>
+      <button
+        onClick={() => {
+          mutate()
+        }}
+      >
+        Upload from CSV
+      </button>
+      <Grid rowData={data} colDefs={colDefs} />
+    </>
+  )
 }
